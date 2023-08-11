1a9326c9d14305f238a43b09c64d7c6385f7f859
hupengwu <5389408@qq.com>
2023-08-11 18:22:24 +0800

    解决发布到maven中央仓库的问题

--- publish2oss.bat
+++ publish2oss.bat
@@ -1,2 +1,11 @@
-#将代码发布到https://s01.oss.sonatype.org/content/repositories/snapshots
+::查看密钥
+gpg --list-keys
+
+::将公钥发布到 PGP 密钥服务器
+gpg --keyserver hkp://pool.sks-keyservers.net --send-keys
+
+::查询公钥是否发布成功
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys
+
+::将代码发布到https://s01.oss.sonatype.org/content/repositories/snapshots
 mvn clean deploy -P release 
