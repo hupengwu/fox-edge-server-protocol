01cd580a6df9ce0b7fc9dd50858da2749ff4fe98
hupengwu <5389408@qq.com>
2023-09-16 15:48:22 +0800

    删除旧的文件

--- fox-edge-server-protocol/fox-edge-server-protocol-core/src/main/java/cn/foxtech/device/protocol/core/constants/FoxEdgeConstant.java
+++
@@ -1,9 +0,0 @@
-package cn.foxtech.device.protocol.core.constants;
-
-public class FoxEdgeConstant {
-    public static final String DATA_TAG = "data";
-
-    public static final String OPERATE_NAME_TAG = "operateName";
-
-    public static final String RECORD_TYPE_TAG = "recordType";
-}
--- fox-edge-server-protocol/fox-edge-server-protocol-omron-fins-core/src/main/java/cn/foxtech/device/protocol/omron/fins/core/entity/pdu/TransferRespond.java
+++
@@ -1,4 +0,0 @@
-package cn.foxtech.device.protocol.omron.fins.core.entity.pdu;
-
-public class TransferRespond extends Transfer{
-}
