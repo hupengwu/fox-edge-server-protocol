523c825d532ee36cc26445fcad7e07cdf5f1b8c8
hupengwu <5389408@qq.com>
2023-10-26 21:31:21 +0800

    保存

--- publish2oss.bat
+++ publish2oss.bat
@@ -1,3 +1,11 @@
+:: 在本地Maven的Settings.xml总，填写sonatype的账号密码
+::	<server>
+::      <id>oss</id>
+::      <username>sonatype的账号</username>
+::      <password>sonatype的密码</password>
+::    </server>
+::
+	
 ::查看密钥
 gpg --list-keys
 
@@ -9,3 +17,5 @@ gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys
 
 ::将代码发布到https://s01.oss.sonatype.org/content/repositories/snapshots
 mvn clean deploy -P release 
+
+:: 上面的编译/提交完成后，再去网站https://s01.oss.sonatype.org/ 手动审批代码，就会发布到中央仓库了
